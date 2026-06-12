e457ccfa887cc64f4f3db158350d7f25c7352d67
isimolina16 <isidora.molina1@mail.udp.cl>
2026-06-12 15:48:28 -0400

    Create solemne-1.js

---
+++ encargos/solemne-1.js
@@ -0,0 +1,59 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+push();
+  
+fill(255,0,0)//color rojo, fill=llenar
+  noStroke();// no trazo/grosor
+triangle(0,0,width,0,0, height)
+fill(255,255,255)//blanco
+triangle(width,height,width,0,0,height)
+
+  angleMode
+  rotate(-70)
+  noStroke();
+rect(50,20,10,300)
+
+rect(0,0,40,300)
+  
+  fill(0,0,0);
+  rect(0,-50,40,300)
+  
+  fill(0,0,0);
+  rect(-50,-100,-60,300)
+  
+  fill(0,0,0);
+  rect(-10,-10,-10,500)
+  
+  fill(0,0,0)
+  rect(-30,-10,-10,500)
+  
+ 
+  fill(255)
+  rect(-75,0,-10,300)
+  
+  //textSize=tamaño de la fuente.
+  //textAlign= forma de alinear el texto.
+  //textStyle=estilo de la fuente.
+  //textFont
+  fill(0,)
+pop()  
+  
+  
+  translate(width/2, height/2)
+  rotate()
+  textSize(13);
+  textAlign(RIGHT);
+  textFont('monospace');
+  text('CONSTRUCCIÓN- es una organización de los elementos',200,200);
+  
+  
+
+  
+
+  
+
+}
